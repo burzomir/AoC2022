a4290cbc649a3aa19a1e64c62725bc223aa79868
burzomir <burzomirdev@gmail.com>
2022-12-02 21:59:39 +0100

    Day0201

---
+++ app/Day02/Day0201.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+
+module Main where
+
+import Text.Parsec
+import Prelude hiding (round)
+
+data Shape
+    = Rock
+    | Paper
+    | Scissors
+    deriving (Show)
+
+data Result
+    = Lost
+    | Draw
+    | Won
+    deriving (Show)
+
+data Player
+    = Opponent
+    | Player
+
+data Move p = Move Shape deriving (Show)
+
+data Round = Round (Move Opponent) (Move Player) Result deriving (Show)
+
+result :: Move Opponent -> Move Player -> Result
+result (Move Rock) (Move Paper) = Won
+result (Move Paper) (Move Scissors) = Won
+result (Move Scissors) (Move Rock) = Won
+result (Move Rock) (Move Rock) = Draw
+result (Move Paper) (Move Paper) = Draw
+result (Move Scissors) (Move Scissors) = Draw
+result _ _ = Lost
+
+playerMove :: Parsec String u (Move Player)
+playerMove = (char 'X' >> return (Move Rock))
+          <|> (char 'Y' >> return (Move Paper))
+          <|> (char 'Z' >> return (Move Scissors))
+
+opponentMove :: Parsec String u (Move Opponent)
+opponentMove = (char 'A' >> return (Move Rock))
+            <|> (char 'B' >> return (Move Paper))
+            <|> (char 'C' >> return (Move Scissors))
+
+round :: Parsec String u Round
+round = do
+    opponent <- opponentMove
+    char ' '
+    player <- playerMove
+    return (Round opponent player (result opponent player))
+
+parseFile :: Parsec String () a -> FilePath -> IO (Either ParseError a)
+parseFile p fp = parse p fp <$> readFile fp
+
+parseInputFile :: FilePath -> IO (Either ParseError [Round])
+parseInputFile = parseFile p'
+  where p' = round `sepBy` endOfLine >>= \ps -> eof >> return ps
+
+
+class Score a where
+    score :: a -> Int
+
+instance Score Shape where
+    score Rock = 1
+    score Paper = 2
+    score Scissors = 3
+
+instance Score Result where
+    score Lost = 0
+    score Draw = 3
+    score Won = 6
+
+instance Score Round where
+    score (Round _ (Move p) r) = score p + score r
+
+main :: IO ()
+main = do
+  input <- parseInputFile "app/Day02/Input.txt"
+  print $ (fmap sum) $ (fmap . fmap) score input

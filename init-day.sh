574d6689cec63231e21519b1bb702e0f3ddfc61f
burzomir <burzomirdev@gmail.com>
2022-12-12 23:07:21 +0100

    Implement day initialization script

---
+++ init-day.sh
@@ -0,0 +1,56 @@
+# init-day.sh <day>
+
+mkdir "./app/Day$1"
+
+cat << EOF > "./app/Day$1/Day$101.hs"
+module Main where
+
+main = print "Hello Day $101"
+
+EOF
+
+cat << EOF > "./app/Day$1/Day$102.hs"
+module Main where
+
+main = print "Hello Day $102"
+
+EOF
+
+touch "./app/Day$1/Input.txt"
+
+cat << EOF >> "./AoC2022.cabal"
+
+executable Day$101
+  main-is:          Day$1/Day$101.hs
+  build-depends:
+    , AoC2022
+    , base
+    , ilist
+    , parsec
+    , split
+    , containers
+    , vector
+    , transformers
+    , search-algorithms
+  hs-source-dirs:   app
+  default-language: Haskell2010
+
+executable Day$102
+  main-is:          Day$1/Day$102.hs
+  build-depends:
+    , AoC2022
+    , base
+    , ilist
+    , parsec
+    , split
+    , containers
+    , vector
+    , transformers
+    , search-algorithms
+  hs-source-dirs:   app
+  default-language: Haskell2010
+
+EOF
+
+cabal run Day$101
+cabal run Day$102
